dd42595a8dd3f9bc61622d05efd4751dbc91e676
unknown <sugarmasters@gmail.com>
2014-12-26 14:59:17 +0100

    chatya listen simple

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var cont = 0;
 
+app..listen(process.env.PORT || 5000)
+
 //Le pasamos el indice a la aplicación
 app.get('/', function(req, res){
 res.sendFile(__dirname + '/index.html');
@@ -15,11 +17,11 @@ io.on('connection', function(socket){
 //Conexión y desconexión de usuarios
 cont++;
 console.log('Usuario conectado');
-console.log('Usuarios conectados:'+ cont);
+console.log('Conectados:'+ cont);
 socket.on('disconnect', function(){
 console.log('Usuario desconectado');
 cont--;
-console.log('Usuarios conectados:'+ cont);
+console.log('Conectados:'+ cont);
 });
 
 socket.on('chat message', function(msg){
@@ -28,6 +30,9 @@ io.emit('chat message', msg);
 });
 });
 
+/* LISTEN VIEJO
 http.listen(3000, function(){
 console.log('listening on *:3000');
 });
+*/
+
