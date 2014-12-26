da49c58a6b3040c632e11f12ff1f5eefdea0ee80
unknown <sugarmasters@gmail.com>
2014-12-26 05:38:02 +0100

    chatya1

---
+++ index.js
@@ -0,0 +1,33 @@
+//Creamos los objetos
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var cont = 0;
+
+//Le pasamos el indice a la aplicación
+app.get('/', function(req, res){
+res.sendFile(__dirname + '/index.html');
+});
+
+//Al conectar a la aplicación...
+io.on('connection', function(socket){
+
+//Conexión y desconexión de usuarios
+cont++;
+console.log('Usuario conectado');
+console.log('Usuarios conectados:'+ cont);
+socket.on('disconnect', function(){
+console.log('Usuario desconectado');
+cont--;
+console.log('Usuarios conectados:'+ cont);
+});
+
+socket.on('chat message', function(msg){
+//console.log(msg);
+io.emit('chat message', msg);
+});
+});
+
+http.listen(3000, function(){
+console.log('listening on *:3000');
+});
